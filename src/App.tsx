ddb4dce611ad06a774b055128e6109f2dd82f502
friche11 <vicfriche@gmail.com>
2024-02-07 15:43:15 -0300

    Rotas para páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
